aa8e24e64369d53565cea252214d70df1758fb67
Joachim Banzhaf <Joachim.Banzhaf@gmail.com>
2023-11-23 19:50:11 +0100

    back to adpcm

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@
 #include <Arduino.h>
 
 #include "AudioTools.h"
-// #include "AudioCodecs/ContainerBinary.h"
-// #include "AudioCodecs/CodecADPCM.h"
+#include "AudioCodecs/ContainerBinary.h"
+#include "AudioCodecs/CodecADPCM.h"
 // can hang #include "AudioCodecs/CodecSBC.h"
 // noise #include "AudioCodecs/CodecAPTX.h"
 // silent errors #include "AudioCodecs/CodecLC3.h"
@@ -122,12 +122,12 @@ private:
 
 I2SStream i2s;  // sink MAX98357A mono amp
 Convert cvt(i2s);  // 1ch -> 2ch as required for the mono amp - go figure... :)
-// BinaryContainerDecoder bcd(new ADPCMDecoder(AV_CODEC_ID_ADPCM_IMA_WAV));
-// EncodedAudioStream dec(&cvt, &bcd);
+BinaryContainerDecoder bcd(new ADPCMDecoder(AV_CODEC_ID_ADPCM_IMA_WAV));
+EncodedAudioStream dec(&cvt, &bcd);
 // can hang EncodedAudioStream dec(&cvt, new BinaryContainerDecoder(new SBCDecoder()));
 // noise EncodedAudioStream dec(&cvt, new BinaryContainerDecoder(new APTXDecoder()));
-// EncodedAudioStream dec(&cvt, new BinaryContainerDecoder(new LC3Decoder()));
-EncodedAudioStream dec(&cvt, new DecoderL8());
+// silent errors EncodedAudioStream dec(&cvt, new BinaryContainerDecoder(new LC3Decoder()));
+// slow EncodedAudioStream dec(&cvt, new DecoderL8());
 StreamCopy copier(dec, Serial1, 1024); 
 
 
@@ -145,7 +145,7 @@ void setup() {
 
   i2s.begin(icfg);
   dec.begin(in);
-  // bcd.setAudioInfo(in);
+  bcd.setAudioInfo(in);
 
   uint8_t rx=15;
   uint8_t tx=22;
